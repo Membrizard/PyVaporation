bc4fba2ed5435cc5006559ab1f405d4941e7594d
Membrizard <denis.sapegin@quantori.com>
2022-05-10 13:04:47 +0300

    Function for calculation of activation energy
    The draft of activation energy function

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
+import typing
+
 import attr
+import numpy
 
 from enum import Enum
 
@@ -58,3 +61,73 @@ class NRTLParameters:
     g12: float
     g21: float
     alpha: float
+
+
+@attr.s(auto_attribs=True)
+class Membrane:
+    ideal_experiments: typing.List[IdealExperiment]
+    non_ideal_experiments: typing.List[NonIdealExperiment]
+
+    @property
+    def ideal_experiments_names(self) -> typing.List[str]:
+        return [ie.component.name for ie in self.ideal_experiments]
+
+    # Get all the penetrants the membrane was tested for
+    def get_known_penetrants(self) -> typing.List[Component]:
+        return numpy.unique([
+            ideal_experiment.component for ideal_experiment in self.ideal_experiments
+        ])
+
+    # Picking only Experiments related to the chosen component
+    def get_penetrant_data(self, component) -> typing.List[IdealExperiment]:
+        return list(
+            filter(
+                lambda value: value.component.name in self.ideal_experiments_names,
+                self.ideal_experiments
+            )
+        )
+
+    # Calculate an apparent activation energy of permeation
+    def calculate_activation_energy(self,component) -> float:
+        
+        self.get_penetrant_data(component)
+        # Calculation of Least-squares Linear Fit of Ln(Permeance) versus 1/T
+        return 0
+
+    def get_permeance(self, temperature, component) -> float:
+        return 0
+
+    def get_ideal_selectivity(self, temperature, component1, component2) -> float:
+        return 0
+
+
+@attr.s(auto_attribs=True)
+class Pervaporation:
+    membrane: Membrane
+    mixture: Mixture
+    conditions: Conditions
+    ideal: bool = True
+
+    def get_component_flux(self):
+        pass
+
+    def get_overall_flux(self):
+        pass
+
+    def get_separation_factor(self):
+        pass
+
+    def get_psi(self):
+        pass
+
+    def get_real_selectivity(self):
+        pass
+    
+    def model_ideal_diffusion_curve(self):
+        pass
+
+    def model_ideal_process(self):
+        pass
+
+    def model_non_ideal_process(self):
+        pass
