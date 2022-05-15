7dbbfe2e167968bfb73bc79788beac3794b194ac
Alexey Chekmachev <a.checkmachev@gmail.com>
2022-05-15 11:36:35 +0300

    code health

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -1,10 +1,16 @@
+import attr
+import typing
+
+from mixture import Mixture, Composition
+
+
 @attr.s(auto_attribs=True)
 class DiffusionCurve:
     feed_temperature: float
-    permeate_temperature: typing.Optional[float] = 0
     mixture: Mixture
     composition_range: typing.List[Composition]
     partial_fluxes: typing.List[typing.List[float]]
     total_flux: typing.List[float]
     separation_factor: typing.List[float]
     PSI: typing.List[float]
+    permeate_temperature: typing.Optional[float] = 0
