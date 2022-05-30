c4d8e74e07db8d88e5fe93b48a714e118aabff2c
Membrizard <denis.sapegin@quantori.com>
2022-05-30 12:41:18 +0300

    Introduced DiffusionCurves

--- experiments/ideal.py
+++ experiments/ideal.py
@@ -13,7 +13,7 @@ from component import AllComponents, Component
 @attr.s(auto_attribs=True)
 class IdealExperiment:
     # temperature: float  # K
-    # permeance: float  # kg * mcm / (m2 * h * kPa)
+    # permeance: float  # kg / (m2 * h * kPa)
     # component: Component
     # activation_energy: typing.Optional[float] = None
 
--- membrane/membrane.py
+++ membrane/membrane.py
@@ -12,7 +12,7 @@ from utils import R
 @attr.s(auto_attribs=True)
 class Membrane:
     ideal_experiments: IdealExperiments
-    diffusion_curves : DiffusionCurves
+    diffusion_curves: DiffusionCurves
     name: str
 
     # non_ideal_experiments: typing.List[NonIdealExperiment]  # TODO: non ideal experiments - Diffusion curves
