3d8e6bd16f16813cf2acee7e0e31ac043537b5c4
Membrizard <denis.sapegin@quantori.com>
2022-06-08 17:44:54 +0300

    Update non_ideal_process.py

--- pervaporation/non_ideal_process.py
+++ pervaporation/non_ideal_process.py
@@ -8,7 +8,12 @@ from diffusion_curve import DiffusionCurve
 from membrane import Membrane
 from mixture import Composition, CompositionType, Mixture, get_nrtl_partial_pressures
 from process import ProcessModel
+from optimizer import PervaporationFunction
 
 
-def non_ideal_model():
+def non_ideal_model(
+        permeance_function_first: PervaporationFunction,
+        permeance_function_second: PervaporationFunction,
+
+):
     return 0
