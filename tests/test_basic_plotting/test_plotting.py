92db0a9c0ee6655f0c2a0f5e02ff4ab48c9cabee
Membrizard <denis.sapegin@quantori.com>
2022-06-27 23:30:04 +0300

    Added code coverage, started plotting tests

---
+++ tests/test_basic_plotting/test_plotting.py
@@ -0,0 +1,22 @@
+from pytest import fixture
+
+from components import Components
+from conditions import Conditions, TemperatureProgram
+from diffusion_curve import DiffusionCurveSet
+from experiments import IdealExperiment, IdealExperiments
+from membrane import Membrane
+from mixtures import Composition, CompositionType, Mixtures
+from permeance import Permeance
+from pervaporation import Pervaporation
+
+
+def test_plot_diffusion_curve():
+    assert 0 == 0
+
+
+def test_plot_pervaporation_function():
+    assert 0 == 0
+
+
+def test_plot_process_model():
+    assert 0 == 0
