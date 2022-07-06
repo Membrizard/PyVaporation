64edb8eb23d92172eb306344afaa0753c815aea3
Alexey Chekmachev <a.checkmachev@gmail.com>
2022-07-06 14:41:41 +0300

    keep fresh python versions only

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: Apache license 2.0",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
