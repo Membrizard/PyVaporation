9e49cb0422743370aed8fae92161ab2d5ef77a2b
Membrizard <denis.sapegin@quantori.com>
2022-07-07 08:24:09 +0300

    refactored PV

--- pervaporation/pervaporation.py
+++ pervaporation/pervaporation.py
@@ -388,6 +388,9 @@ class Pervaporation:
                 )
             )
 
+        feed_mass.pop(-1)
+        feed_composition.pop(-1)
+
         return ProcessModel(
             mixture=self.mixture,
             membrane_name=self.membrane.name,
@@ -571,6 +574,9 @@ class Pervaporation:
                 feed_temperature.append(
                     conditions.temperature_program.program(time[step] + delta_hours)
                 )
+
+        feed_mass.pop(-1)
+        feed_composition.pop(-1)
 
         return ProcessModel(
             mixture=self.mixture,
