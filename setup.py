28673b438b90e421e8d73c0d0a59eeb927fe7323
Alexey Chekmachev <a.checkmachev@gmail.com>
2022-07-15 14:00:55 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="pyvaporation",
     packages=find_packages(),
-    version="1.1.2",
+    version="1.1.3",
     license="Apache license 2.0",
     description="Set of tools for modelling pervaporation processes",
     author="Denis Sapegin, Aleksei Chekmachev",
     author_email="a.checkmachev@gmail.com",
     url="https://github.com/Membrizard/PyVaporation",
-    download_url="https://github.com/Membrizard/PyVaporation/archive/refs/tags/v1.1.2.tar.gz",
+    download_url="https://github.com/Membrizard/PyVaporation/archive/refs/tags/v1.1.3.tar.gz",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     keywords=[
@@ -19,10 +19,12 @@ setup(
         "modelling",
     ],
     install_requires=[
-        "joblib>=1.1.0",
-        "matplotlib>=3.5.2",
-        "pandas>=1.3.5",
-        "scipy>=1.7.3",
+        "joblib==1.1.0",
+        "matplotlib==3.5.2",
+        "pandas==1.3.5",
+        "scipy==1.7.3",
+        "attr==0.3.1",
+        "attrs==21.4.0",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
