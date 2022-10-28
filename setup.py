6acebed3ffa590c6d592655f00fd8a31eb44281d
Denis Sapegin <79135791+Membrizard@users.noreply.github.com>
2022-10-28 12:44:05 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name="pyvaporation",
     packages=find_packages(),
-    version="1.1.4",
+    version="1.1.5",
     license="Apache license 2.0",
     description="Set of tools for modelling pervaporation processes",
     author="Denis Sapegin, Aleksei Chekmachev",
     author_email="a.checkmachev@gmail.com",
     url="https://github.com/Membrizard/PyVaporation",
-    download_url="https://github.com/Membrizard/PyVaporation/archive/refs/tags/v1.1.4.tar.gz",
+    download_url="https://github.com/Membrizard/PyVaporation/archive/refs/tags/v1.1.5.tar.gz",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     keywords=[
